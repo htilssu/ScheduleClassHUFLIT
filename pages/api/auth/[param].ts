e12e79c10a07eb4f5aacfd57aab6e6ba064c6c0e
htilssu <tolashuu@gmail.com>
2024-04-03 01:42:45 +0700

    config vercel.json

--- pages/api/auth/[param].ts
+++ pages/api/auth/[param].ts
@@ -41,6 +41,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         }
     } catch (e) {
+        console.log(e)
         res.status(500).json({message: "Lỗi hệ thống"})
     }
 
