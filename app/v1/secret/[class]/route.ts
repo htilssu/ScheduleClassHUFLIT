7b3a0029ddb2d301bdb7946c7a2322c1e441804e
htilssu <tolashuu@gmail.com>
2024-11-22 03:57:08 +0700

    fix

--- app/v1/secret/[class]/route.ts
+++ app/v1/secret/[class]/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest, {params}: { params: Promise<{ cl
 
     const classSecret = await prisma.classSecret.findFirst({
         where: {
-            classId: pr.class
+            id: pr.class
         }
     })
 
