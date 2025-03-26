049464d0012c501d377bdc5edf9fedcf9ae290c0
Trung Hieu <81826098+htilssu@users.noreply.github.com>
2025-03-27 00:20:23 +0700

    Feature/reset timeline (#29)
    * ♻️ (app): refactor layout structure to improve code organization
    ✨ (app): add share schedule feature
    
    * ✨ Create share schedule page with timeline component
    
    * ✨ (schedule): enhance loading and share schedule features
    🐛 (TimeLine.tsx): remove unnecessary console log
    ✨ (share/[schedule_id]/page.tsx): pass scheduleId to ShareTimelinePage
    ✨ (ShareTimelinePage.tsx): fetch class data based on scheduleId
    ✨ (Loading.tsx): remove dynamic export and set loading to true
    
    * ✨ (app): Implement shareable schedule feature
    
    This commit introduces the shareable schedule feature, allowing users to share their schedules with others.
    
    The following changes were made:
    
    - Added a new route `app/(no-layout)/schedule/share/[schedule_id]/page.tsx` to handle the shared schedule page.
    - Created a new component `app/(no-layout)/schedule/share/components/ShareTimelinePage.tsx` to display the shared schedule.
    - Implemented a new service `lib/service/timeline.ts` to fetch the timeline data from the database.
    - Updated the `TimeLine` model in `prisma/schema.prisma` to store the schedule data as a JSON object.
    - Removed unused code and files related to fetching class data.
    - Added a loading component to improve the user experience.
    
    * ✨ (app): implement timeline state management with Redux
    
    This commit introduces Redux for managing the timeline state,
    allowing for adding, removing, and resetting classes. It also
    integrates local storage for persistence and adds a reset
    timeline feature in the filter bar.
    
    * ♻️ Refactor: rename ShareTimelinePage to ShareTimeLinePage and update Next.js version
    
    * 🐛 (code-type/route.ts): remove unused POST method
    
    * 🐛 (code.service.ts, codetype.service.ts): comment out prisma.code.create and prisma.codeType.create to prevent errors
    
    * ✨ (major/route.ts): disable POST method and cache for GET method

--- app/v1/major/route.ts
+++ app/v1/major/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {Major, PrismaClient} from "@prisma/client";
 import {unstable_cacheLife as cacheLife} from 'next/cache'
+import {cacheTag} from "next/dist/server/use-cache/cache-tag";
 
 const prisma = new PrismaClient();
 
@@ -9,21 +10,25 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(data);
 }
 
+/*
 export async function POST(req: NextRequest) {
     const major = await req.json() as Major;
     const newMajor = await createMajor(major);
     return NextResponse.json(newMajor);
 }
+*/
 
 const getMajor = async () => {
     'use cache';
     cacheLife("minutes")
+    cacheTag('major')
 
     return prisma.major.findMany();
 }
 
+/*
 async function createMajor(major: Major) {
     return prisma.major.create({
         data: major
     });
-}
+}*/
