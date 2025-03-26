2c82c3304dde54fc502b7c19e0d11edc0050dc7b
htilssu <tolashuu@gmail.com>
2025-03-26 23:59:48 +0700

    ♻️ Refactor: rename ShareTimelinePage to ShareTimeLinePage and update Next.js version

--- app/(no-layout)/schedule/share/[schedule_id]/page.tsx
+++ app/(no-layout)/schedule/share/[schedule_id]/page.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import ShareTimelinePage from "@/app/(no-layout)/schedule/share/components/ShareTimelinePage";
+import ShareTimeLinePage from "@/app/(no-layout)/schedule/share/components/ShareTimeLinePage";
 import {Metadata} from 'next';
 import {SearchParams} from '@/app/page.type';
-import {getTimeLine} from '@/lib/service/timeline';
-import {ClassData} from '@/lib/types';
-import {notFound} from 'next/navigation';
-import { Prisma } from '@prisma/client';
 
 
 export const metadata: Metadata = {
@@ -15,15 +11,11 @@ export const metadata: Metadata = {
 
 async function Page({params}: { params: Promise<{ schedule_id: string }>, searchParams: SearchParams }) {
     const {schedule_id: timeLineId} = await params;
-    const timeline = await getTimeLine(timeLineId);
 
-    if (!timeline) {
-        return notFound();
-    }
 
     return (
         <>
-            <ShareTimelinePage classes={timeline!.data as Prisma.JsonArray}/>
+            <ShareTimeLinePage classes={[]}/>
         </>
     );
 }
