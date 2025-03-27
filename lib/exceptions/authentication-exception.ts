34789027a4f2889965679dc4c436e22510ec0f1d
htilssu <tolashuu@gmail.com>
2025-03-27 18:44:38 +0700

    feat(authentication): use AuthenticateErrorCode enum for error codes

--- lib/exceptions/authentication-exception.ts
+++ lib/exceptions/authentication-exception.ts
@@ -2,7 +2,7 @@ import {CredentialsSignin} from "next-auth";
 import {AuthenticateErrorCode} from "@/lib/types";
 
 export class UserNotFoundException extends CredentialsSignin {
-    code: string = "user_not_found"
+    code: AuthenticateErrorCode = "user_not_found";
 }
 
 export class WrongPasswordException extends CredentialsSignin {
