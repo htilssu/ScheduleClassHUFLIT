dc536eb7fca44cd8495749bbb2fc89316a430753
TuanAnh <tuanmeo980provip@gmail.com>
2025-03-27 11:01:11 +0700

    fix responsive Home

--- app/(layout)/home/page.tsx
+++ app/(layout)/home/page.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
 const Page = () => {
     return (
         <div className={"mb-20"}>
-            <div className="flex max-w-7xl 2xl:justify-center xl:justify-start mx-auto mt-5 lg:gap-5 gap-0">
-                <div className="w-full lg:w-[68%] xl:w-[72%]">
+            <div className="flex max-w-7xl justify-start mx-auto mt-5 lg:gap-5 gap-0">
+                <div className="w-full lg:w-[70%] xl:w-[70%] 2xl:w-[70%]">
                     <MainContent/>
                 </div>
-                <div className="xl:w-[26%] w-[28%] fixed xl:right-16 right-8 top-22">
+                <div className="xl:w-[25%] 2xl:[29%] w-[26%] fixed xl:right-16 right-8 2xl:left-2/3 top-22">
                     <Notify/>
                 </div>
             </div>
