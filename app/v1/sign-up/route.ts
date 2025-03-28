0765f134551721d4542eefaac877227136f8e933
htilssu <tolashuu@gmail.com>
2025-03-28 14:52:21 +0700

    feat: implement sign-up functionality and remove username unique constraint
    This commit introduces the sign-up functionality, allowing new users to create accounts. It includes the creation of a new route for handling sign-up requests, hashing passwords for security, and storing user data in the database. Additionally, the unique constraint on the username field has been removed from the User model in the Prisma schema.

---
+++ app/v1/sign-up/route.ts
@@ -0,0 +1,32 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/lib/service/prismaClient";
+import {hashPassword} from "@/lib/utils/password";
+import {nanoid} from "nanoid";
+
+export async function POST(req: NextRequest) {
+    const body = await req.json() as { [a: string]: string };
+    const user = await prisma.user.findFirst({
+        where: {
+            OR: [
+                {username: body.username},
+                {email: body.username}
+            ]
+        }
+    });
+
+    if (user) {
+        return NextResponse.json({message: "Invalid credentials"}, {status: 401});
+    }
+
+    const hash = await hashPassword(body.password);
+
+    const {email, password, name} = body;
+    const createdUser = await prisma.user.create({
+        data: {
+            email,
+            username: nanoid(8),
+            password: hash,
+            name
+        }
+    })
+}
