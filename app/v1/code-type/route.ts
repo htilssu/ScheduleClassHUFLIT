1dcfe01e83a214f2b0e91f0cf8670d1a9a3781cc
htilssu <tolashuu@gmail.com>
2025-04-07 14:31:57 +0700

    Update sitemap URL and remove unused code files
    - Changed the sitemap URL in `robots.ts` to point to the new domain.
    - Deleted `route.ts` and `code-type/route.ts` files as they are no longer in use, improving codebase cleanliness and maintainability.
    - Ensured proper documentation for any remaining functions and variables to assist future developers.

--- app/v1/code-type/route.ts
+++
@@ -1,9 +0,0 @@
-import {NextRequest, NextResponse} from "next/server";
-import {CodeTypeService} from "@/lib/service/code_type";
-
-export async function POST(request: NextRequest) {
-    /*const codeType: CodeType = await request.json();
-    const createdCodeType = await CodeTypeService.createCodeType(codeType);
-
-    return NextResponse.json(createdCodeType);*/
-}
